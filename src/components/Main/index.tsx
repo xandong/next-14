7c1ddc0d0239098ce4bd06e54f7aa441ce67f3bd
Alexandre <xandongurgel@gmail.com>
2024-10-09 20:24:04 -0300

    Added initial Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 const Main = () => {
   return (
-    <main>
-      <h1>Main Component</h1>
+    <main className="h-[100vh] bg-[#0A0A0A] flex justify-center items-center">
+      <h1 className="text-white text-5xl font-bold">Main Component</h1>
     </main>
   )
 }
